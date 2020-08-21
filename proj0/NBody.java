dedb0fd3db4cae6a01729ab248d1115ca4400111
Gaurav Joshi <joshi.gaurav.0910@gmail.com>
2020-08-20 23:42:42 -0500

    Completed ReadRadius and ReadPlanets methods for NBody.java

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,8 +1,37 @@
 public class NBody {
+    public static double readRadius(String filePath) {
+        In inputFile = new In(filePath);
+        inputFile.readInt();
 
-    
+        double radius = inputFile.readDouble(); 
 
+        return radius;
+    }
 
+    public static Planet[] readPlanets(String filePath) {
+        In inputFile = new In(filePath);
+        Planet[] planets = new Planet[5];
 
+        inputFile.readInt();
+        inputFile.readDouble();
+        
+        int index = 0;
 
+        while(index < 5) {
+
+            double xxPositionString = inputFile.readDouble();
+            double yyPositionString = inputFile.readDouble();
+            double xxVelocityString = inputFile.readDouble();
+            double yyVelocityString = inputFile.readDouble();
+            double mass = inputFile.readDouble();
+            String image = inputFile.readString();
+            planets[index] = new Planet(xxPositionString, yyPositionString, 
+                                    xxVelocityString, yyVelocityString, 
+                                    mass, image);
+
+            index++;
+        }
+
+        return planets;
+    }
 }
